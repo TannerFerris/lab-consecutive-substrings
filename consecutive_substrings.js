6afeb0333b08b6a1b2a378a8ff6c83a799069f36
Tanner Foster <tannerfoster@ferristexas.gov>
2025-03-20 13:01:38 -0500

    Submission

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,14 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  let substrings = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      substrings.push(string.slice(i, j));
+    }
+}
+return substrings;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
